2fa550ff4235c185d10d7e357dfaad3e7ec3b567
Liz <42483589+Krunniee@users.noreply.github.com>
2018-10-05 11:04:46 -0400

    host refactor GO!

---
+++ api/main.js
@@ -0,0 +1,8 @@
+app.connect() {
+  if (!app) return console.error("Error While Connecting");
+  app.on("Started On " + Date.UTC)
+}
+
+app.connection() {
+  if (app.error) return console.error("There Was An Error During Processing");
+}
